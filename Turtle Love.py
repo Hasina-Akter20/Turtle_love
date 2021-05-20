0effddd5ad31247dbb726402b4837f12830ddfc3
Hasina Akter <66660517+Hasina-Akter20@users.noreply.github.com>
2021-05-20 12:05:15 +0600

    Add files via upload

---
+++ Turtle Love.py
@@ -0,0 +1,14 @@
+import turtle
+
+
+turtle.begin_fill()
+turtle.color("red")
+turtle.left(45)
+turtle.forward(180)
+turtle.circle(90,180)
+turtle.setheading(135)
+turtle.circle(90,180)
+turtle.forward(180)
+turtle.end_fill()
+
+turtle.mainloop()
